18b67d002e9c1aec1a53a99cfa3b80f130786fda
cristian-castrillon <cristian.castrillon@gmail.com>
2017-08-02 16:34:01 -0500

    [ADD] Create, show in question page and errors for answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :answers
-  resources :questions, except: [:edit, :update, :destroy]
+  resources :questions, except: [:edit, :update, :destroy] do
+    resources :answers, only: [:create]
+  end
+  get 'questions/:id/answers', to: 'questions#show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'questions#index'
