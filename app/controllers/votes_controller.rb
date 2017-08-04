c35e88e5834d4c13c104d02b64837c1faac8e632
cristian-castrillon <cristian.castrillon@gmail.com>
2017-08-04 15:11:19 -0500

    [ADD] Votes for questions

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,23 @@
 class VotesController < ApplicationController
+  def create
+    if params[:question_id]
+      question = Question.find(params[:question_id])
+      question.votes.create(user: current_user)
+    else
+      puts "Parameters= #{params}"
+      redirect_to(root_path)
+    end
+    redirect_to(question)
+  end
+
+  def destroy
+    if params[:question_id]
+      question = Question.find(params[:question_id])
+      question.votes.where(user: current_user).take.try(:destroy)
+    else
+      puts "Parameters= #{params}"
+      redirect_to(root_path)
+    end
+    redirect_to(question)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :questions, except: [:edit, :update, :destroy] do
     resources :answers, only: [:create]
     resources :comments, only: [:create]
+    resource :vote, only: [:create, :destroy]
   end
   resources :answers, only: [:index] do
     resources :comments, only: [:create]
+    resource :vote, only: [:create, :destroy]
   end
   get 'questions/:id/answers', to: 'questions#show'
   get 'questions/:id/comments', to: 'questions#show'
