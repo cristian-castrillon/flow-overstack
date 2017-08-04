b262cf8726e7e5e9ffa03e703f735e3a9dba0491
cristian-castrillon <cristian.castrillon@gmail.com>
2017-08-04 15:52:43 -0500

    [ADD] Votes to the answers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,8 +4,9 @@ class VotesController < ApplicationController
       question = Question.find(params[:question_id])
       question.votes.create(user: current_user)
     else
-      puts "Parameters= #{params}"
-      redirect_to(root_path)
+      answer = Answer.find(params[:answer_id])
+      question = Question.find(answer.question_id)
+      answer.votes.create(user: current_user)
     end
     redirect_to(question)
   end
@@ -15,8 +16,9 @@ class VotesController < ApplicationController
       question = Question.find(params[:question_id])
       question.votes.where(user: current_user).take.try(:destroy)
     else
-      puts "Parameters= #{params}"
-      redirect_to(root_path)
+      answer = Answer.find(params[:answer_id])
+      question = Question.find(answer.question_id)
+      answer.votes.where(user: current_user).take.try(:destroy)
     end
     redirect_to(question)
   end
